7b58476b519ab58cba83b534c1dfa6c5b2ee09a1
LucaOcchio99 <occhionorelli.luca99@gmail.com>
2021-12-10 17:12:54 +0100

    draft

--- src/components/ServicesCard.vue
+++ src/components/ServicesCard.vue
@@ -3,21 +3,73 @@
     
     <div class="superior_cards">
        <div class="card">
-          
+          <div class="icone">
+             <i class="fas fa-desktop"></i>
+             <i class="fas fa-arrow-right"></i>
+          </div>
+          <div>
+             <h2>Audit  Assurance</h2>
+             <p>Lorem ipsum dolor sit amet <br> consectetur adipiscing elit.</p>
+          </div>
        </div>
 
-       <div class="card"></div>
+       <div class="card">
+         <div class="icone">
+             <i class="fas fa-desktop"></i>
+             <i class="fas fa-arrow-right"></i>
+          </div>
+          <div>
+             <h2>Financial Advisory</h2>
+             <p>Lorem ipsum dolor sit amet <br> consectetur adipiscing elit.</p>
+          </div>
+       </div>
 
-       <div class="card"></div>
+       <div class="card">
+         <div class="icone">
+             <i class="fas fa-desktop"></i>
+             <i class="fas fa-arrow-right"></i>
+          </div>
+          <div>
+             <h2>Analytics and M&amp;A</h2>
+             <p>Lorem ipsum dolor sit amet <br> consectetur adipiscing elit.</p>
+          </div>
+       </div>
     </div>
 
     <div class="lower_cards">
 
-        <div class="card"></div>
+        <div class="card">
+          <div class="icone">
+             <i class="fas fa-desktop"></i>
+             <i class="fas fa-arrow-right"></i>
+          </div>
+          <div>
+             <h2>Middle Marketing</h2>
+             <p>Lorem ipsum dolor sit amet <br> consectetur adipiscing elit.</p>
+          </div>
+        </div>
 
-        <div class="card"></div>
+        <div class="card">
+          <div class="icone">
+             <i class="fas fa-desktop"></i>
+             <i class="fas fa-arrow-right"></i>
+          </div>
+          <div>
+             <h2>Legal Consulting</h2>
+             <p>Lorem ipsum dolor sit amet <br> consectetur adipiscing elit.</p>
+          </div>
+        </div>
 
-        <div class="card"></div>
+        <div class="card">
+          <div class="icone">
+             <i class="fas fa-desktop"></i>
+             <i class="fas fa-arrow-right"></i>
+          </div>
+          <div>
+             <h2>Regulator Risk</h2>
+             <p>Lorem ipsum dolor sit amet <br> consectetur adipiscing elit.</p>
+          </div>
+        </div>
     </div>
 
   </section>
@@ -36,10 +88,15 @@ name: 'ServicesCard',
    justify-content: space-between;
     
     .card {
-        height: 250px;
-        width: 300px;
         border: 1px solid black;
+        width: 300px;
+        padding: 3rem;
     }
+}
+
+.icone {
+  display: flex;
+  justify-content: space-between;
 }
 
 .lower_cards {
